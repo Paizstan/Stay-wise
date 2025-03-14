4ba9aac52c7ed5064d2202283960f6a64b6a35f7
Stanaley Paiz <ps24001@itcha.edu.sv>
2025-03-13 21:58:08 -0600

    Actualización de dependencias y mejoras en Habitaciones.vue y app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,41 +1,41 @@
-import '../css/app.css';
-import './bootstrap';
+import "../css/app.css";
+import "./bootstrap";
 
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createApp, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import PrimeVue from 'primevue/config';
-import ToastService from 'primevue/toastservice';
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { createApp, h } from "vue";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy";
+import PrimeVue from "primevue/config";
+import ToastService from "primevue/toastservice";
 
 // Import PrimeVue components
-import Toast from 'primevue/toast';
-import Button from 'primevue/button';
-import Toolbar from 'primevue/toolbar';
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-import Dialog from 'primevue/dialog';
-import InputText from 'primevue/inputtext';
-import IconField from 'primevue/iconfield';
-import InputIcon from 'primevue/inputicon';
-import FileUpload from 'primevue/fileupload';
-//import Select from 'primevue/select';
-import Textarea from 'primevue/textarea';
-import InputNumber from 'primevue/inputnumber';
+import Toast from "primevue/toast";
+import Button from "primevue/button";
+import Toolbar from "primevue/toolbar";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import Dialog from "primevue/dialog";
+import InputText from "primevue/inputtext";
+import IconField from "primevue/iconfield";
+import InputIcon from "primevue/inputicon";
+import FileUpload from "primevue/fileupload";
+import Select from 'primevue/select';
+import Textarea from "primevue/textarea";
+import InputNumber from "primevue/inputnumber";
 
-// Import PrimeVue CSS
+//Import PrimeVue CSS
 /* import 'primevue/resources/themes/saga-blue/theme.css'; // or any other theme
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css'; */
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css"; */
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
+            import.meta.glob("./Pages/**/*.vue")
         ),
     setup({ el, App, props, plugin }) {
         const app = createApp({ render: () => h(App, props) });
@@ -45,23 +45,23 @@ createInertiaApp({
         app.use(ToastService);
 
         // Register PrimeVue components
-        app.component('Toast', Toast);
-        app.component('Button', Button);
-        app.component('Toolbar', Toolbar);
-        app.component('DataTable', DataTable);
-        app.component('Column', Column);
-        app.component('Dialog', Dialog);
-        app.component('InputText', InputText);
-        app.component('IconField', IconField);
-        app.component('InputIcon', InputIcon);
-        app.component('FileUpload', FileUpload);
-        //app.component('Select', Select);
-        app.component('Textarea', Textarea);
-        app.component('InputNumber', InputNumber);
+        app.component("Toast", Toast);
+        app.component("Button", Button);
+        app.component("Toolbar", Toolbar);
+        app.component("DataTable", DataTable);
+        app.component("Column", Column);
+        app.component("Dialog", Dialog);
+        app.component("InputText", InputText);
+        app.component("IconField", IconField);
+        app.component("InputIcon", InputIcon);
+        app.component("FileUpload", FileUpload);
+        app.component('Select', Select);
+        app.component("Textarea", Textarea);
+        app.component("InputNumber", InputNumber);
 
         app.mount(el);
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
 });
